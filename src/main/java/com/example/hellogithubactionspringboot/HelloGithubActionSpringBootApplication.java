afa69695ae54f8df998f7442ca2f46171481f5a1
Gerberg1 <masi0001@stud.kea.dk>
2024-02-27 09:57:09 +0100

    Initial commit

---
+++ src/main/java/com/example/hellogithubactionspringboot/HelloGithubActionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubActionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloGithubActionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellogithubactionspringboot/HelloGithubActionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellogithubactionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGithubActionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
